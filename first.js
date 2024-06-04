fdba085249b27609039cd5f1b1391bbce8a38b1d
anbanshayan <anbanshayan3@gmail.com>
2024-06-05 03:32:49 +0530

    getelementByID

--- first.js
+++ first.js
@@ -2,7 +2,29 @@ function displayDate(){
     const element = document.getElementById("date");
     element.innerHTML=new Date()
 }
-let time=
+//function sayGreetings goes here!!!
+const d = new Date()
+
+const h  = d.getHours()
+const m = d.getMinutes()
+const s = d.getSeconds()
+
+document.getElementById("hour").innerHTML = h;
+document.getElementById("minute").innerHTML = m;
+document.getElementById("second").innerHTML = s;
+
 function sayGreetings(){
+    if(0<h<12){
+        document.getElementById("greetings").innerHTML="Good morning"
+    }
+    else if(12<h<16){
+        document.getElementById("greetings").innerHTML="Good afternoon"
     
-}
+    }
+    else{
+        document.getElementById("greetings").innerHTML="Good evening"
+    
+    }
+    }
+////function sayGreetings ends here!!!
+
