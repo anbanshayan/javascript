44783bf3bf89c9b4679f1c2ec5ee5e9cc4d3580a
anbanshayan <anbanshayan3@gmail.com>
2024-06-11 22:35:36 +0530

    var sample declared

---
+++ array.js
@@ -0,0 +1,10 @@
+// You can create a constant array:
+const universities = ["UOC","UOM","USJ","UOK","UOP","UOJ"]
+
+// You can create a constant array:
+//const cars = ["Saab", "Volvo", "BMW"];
+
+universities.push ="UWU";
+
+
+console.log(universities)
