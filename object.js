070b3e0ad114f4f8df25ff685cb6c2a12068c654
anbanshayan <anbanshayan3@gmail.com>
2024-06-20 00:16:20 +0530

    object created using new keyword

--- object.js
+++ object.js
@@ -1,1 +1,18 @@
-const
+const person ={
+    sname:"Anbashayan",
+    age:24,
+    university:"University of Colombo School of Computing",
+    home:"Jaffna"
+}
+console.log(person.sname,person.home)
+
+//we can create object using 'new' keyword
+
+const school = new Object()
+
+school.name = "University of Colombo School of Computing"
+school.year = 2002;
+school.location = "35, Reid Avenue Colombo 7"
+school.director = 'Dr.Ajantha Atukorale'
+
+console.log("Our Campus is "+school.name)
